6b13737b452dd2126fb5b2a05579343adbf4165a
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2022-10-15 12:59:39 -0300

    Add Animated Container sample

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # animated_container
+
+<div align="center">
+  <img src="https://github.com/phferreira/assets/blob/master/gifs/animated_container.gif"/>
+</div>
+
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animated Container',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 45),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Container'),
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              _selected = !_selected;
+            });
+          },
+          child: AnimatedContainer(
+            duration: const Duration(seconds: 1),
+            height: _selected ? 200 : 100,
+            width: _selected ? 200 : 100,
+            alignment: _selected ? Alignment.topLeft : Alignment.bottomRight,
+            decoration: BoxDecoration(
+              color: _selected ? Colors.redAccent : Colors.deepPurple,
+              borderRadius: BorderRadius.circular(_selected ? 100 : 10),
+              // shape: _selected ? BoxShape.circle : BoxShape.rectangle,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
